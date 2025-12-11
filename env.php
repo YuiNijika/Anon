3fb8bb28fc8880df20e03898a4d241bfa0bf7bbc
鼠子 <Tomori@miomoe.cn>
2025-12-11 22:34:32 +0800

    Update env.php

--- env.php
+++ env.php
@@ -13,12 +13,12 @@ return [
             'database' => 'puxt', // 数据库名称
             'charset' => 'utf8mb4', // 数据库字符集
         ],
-        'installed' => true, // 是否安装程序
+        'installed' => false, // 是否安装程序
     ],
     'app' => [
         'debug' => [
             'global' => false, // 全局调试
-            'router' => true, // 路由调试
+            'router' => false, // 路由调试
         ],
     ],
 ];
