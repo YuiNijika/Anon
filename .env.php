1098cf890d49ef5e2d5d0d5454325d4c1d7a3bf7
鼠子 <165134730+YuiNijika@users.noreply.github.com>
2026-01-08 10:06:59 +0800

    Create .env.php

---
+++ .env.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Anon 环境配置
+ * 仅包含数据库配置和安装状态
+ * 其他配置请在 app/useApp.php 中设置
+ */
+
+if (!defined('ANON_ALLOWED_ACCESS')) exit;
+
+// 数据库配置
+define('ANON_DB_HOST', 'localhost');
+define('ANON_DB_PORT', 3306);
+define('ANON_DB_PREFIX', 'anon_');
+define('ANON_DB_USER', 'root');
+define('ANON_DB_PASSWORD', '');
+define('ANON_DB_DATABASE', '');
+define('ANON_DB_CHARSET', 'utf8mb4');
+
+// APP Key
+define('ANON_APP_KEY', '');
+
+// 安装状态
+define('ANON_INSTALLED', false);
